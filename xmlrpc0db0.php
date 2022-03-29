138fd5b65003ad932c71501690c6386edbae172e
Mohammedjaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2022-03-29 12:25:15 +0530

    1st commitment

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://themesflat.com/carenowwp</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://themesflat.com/carenowwp/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://themesflat.com/carenowwp/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://themesflat.com/carenowwp/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://themesflat.com/carenowwp/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://themesflat.com/carenowwp/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
